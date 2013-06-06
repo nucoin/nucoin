2e7e4a60324e3bf056665ffa1b4a60f8f41aeaeb
root <root@hal-VirtualBox.(none)>
2013-06-05 22:09:06 -0600

    Fixed checkpoints Thanks Shakezula

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -25,8 +25,6 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of // Yo dawg, this is the secret. Checkpoint 0 hash == Genesis block hash.
         (         0, uint256("0xd592e6110f92acc63899d45070221eaa5d75ec23950387d3d7595f6a61d178b2"))
-        (        50, unit256("0x53f893c43e4a94d3350ed03cf30da535e8e14109db262ea31dd79d6f5f5cd022"))
-        (       100, unit256("0x33b4eeb1abd2fe62d0c400c17fd3fc0adc89e8803c8a74cdc1efc5f9299c393b"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
